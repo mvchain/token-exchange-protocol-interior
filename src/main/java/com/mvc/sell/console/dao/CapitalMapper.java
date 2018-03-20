3660d8335dbf0897f037ba5760d42f8265d96256
戚羿辰 <416350144@qq.com>
2018-03-20 17:21:04 +0800

    update sql

--- src/main/java/com/mvc/sell/console/dao/CapitalMapper.java
+++ src/main/java/com/mvc/sell/console/dao/CapitalMapper.java
@@ -18,7 +18,7 @@ import java.util.List;
  * @create 2018/3/13 17:31
  */
 public interface CapitalMapper extends Mapper<Capital> {
-    @Select("select t1.*, t2.* from capital t1, config t2 where t2.id = t1.token_id and t1.user_id = #{userId} and t2.show = 1")
+    @Select("select t1.*, t2.* from capital t1, config t2 where t2.id = t1.token_id and t1.user_id = #{userId} and t2.need_show = 1")
     List<CapitalVO> selectBalance(Capital capital);
 
     @Update("update capital set balance = balance + #{balance} where user_id = #{userId} and token_id = #{tokenId}")
