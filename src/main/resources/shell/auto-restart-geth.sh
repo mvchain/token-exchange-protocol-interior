7fe54973c557649f9f2a87674040bbff506d32c1
YWD@MVC <memorywilllast@gmail.com>
2018-03-21 20:46:11 +0800

    auto restart geth shell script.

--- src/main/resources/shell/auto-restart-geth.sh
+++ src/main/resources/shell/auto-restart-geth.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 kill -9 `ps -ef | grep "geth --rpc"|egrep -v "grep"|awk '{print $2}'`
-nohup
+nohup geth --rpc --rpcaddr 127.0.0.1 --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" > /opt/log/geth.log 2>&1 &
