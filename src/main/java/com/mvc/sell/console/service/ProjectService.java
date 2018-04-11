38bab827662d1f1709b4356d8e95191c2c3f405f
戚羿辰 <416350144@qq.com>
2018-04-11 14:32:35 +0800

    fix bug

--- src/main/java/com/mvc/sell/console/service/ProjectService.java
+++ src/main/java/com/mvc/sell/console/service/ProjectService.java
@@ -295,9 +295,13 @@ public class ProjectService extends BaseService {
         Assert.isTrue(canRetire, MessageConstants.CANNOT_RETIRE);
         project.setRetire(1);
         projectMapper.updateByPrimaryKeySelective(project);
-        projectMapper.retireBalance(id);
-        projectMapper.retireToken(id, config.getId());
-        orderService.retireToken(id, CommonConstants.ORDER_STATUS_RETIRE);
+        Orders orders = new Orders();
+        orders.setProjectId(id);
+        if (orderMapper.selectCount(orders) > 0){
+            projectMapper.retireBalance(id);
+            projectMapper.retireToken(id, config.getId());
+            orderService.retireToken(id, CommonConstants.ORDER_STATUS_RETIRE);
+        }
     }
 
     public List<Project> select(Project project) {
